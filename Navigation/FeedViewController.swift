030b9dc9935e9709170d678d5b1d3cbcd3a8f57b
Ivan Kamenev <lkamenev92@gmail.com>
2021-03-26 11:06:35 +0300

    commit for working

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -7,7 +7,112 @@
 //
 
 import UIKit
+import SnapKit
 
+protocol FeedViewOutput {
+    func showPost(_:Post)
+    var navigationController: UINavigationController? { get set }
+}
+
+    class FeedViewController: UIViewController {
+
+        var output: FeedViewOutput?
+
+        lazy var containerView: UIView = {
+            let container = ContainerView()
+            container.onTap = output?.showPost(_:)
+            return container
+        }()
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.backgroundColor = .green
+            output = PostPresenter()
+            output?.navigationController = navigationController
+            view.addSubview(containerView)
+            setupConstraints()
+            print(type(of: self), #function)
+
+        }
+        
+        func setupConstraints() {
+            containerView.snp.makeConstraints() { make in
+                make.top.leading.bottom.trailing.equalToSuperview()
+            }
+        }
+
+    }
+    class ContainerView: UIView {
+
+        let post: Post = Post(title: "Post")
+
+        var onTap: ((Post) -> Void)?
+
+        let oneButton: UIButton = {
+            let button = UIButton(type: .system)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle("PostVC", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .systemRed
+            button.addTarget(self, action: #selector(buttonTap), for: .touchUpInside)
+            return button
+        }()
+        
+        let twoButton: UIButton = {
+            let button = UIButton(type: .system)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle("PostVC", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .systemRed
+            button.addTarget(self, action: #selector(buttonTap), for: .touchUpInside)
+            return button
+        }()
+
+        lazy var buttonsStack: UIStackView = {
+            let buttonsStack = UIStackView()
+            buttonsStack.addArrangedSubview(oneButton)
+            buttonsStack.addArrangedSubview(twoButton)
+            buttonsStack.alignment = .fill
+            buttonsStack.distribution = .fillEqually
+            buttonsStack.axis = .horizontal
+            buttonsStack.spacing = 10
+            buttonsStack.layer.masksToBounds = true
+            return buttonsStack
+        }()
+
+        func newConstraints() {
+                buttonsStack.snp.makeConstraints() { make in
+                    make.leading.equalToSuperview().offset(16)
+                    make.trailing.equalToSuperview().inset(16)
+                    make.centerY.equalToSuperview()
+                }
+            }
+
+        @objc private func buttonTap() {
+            guard onTap != nil else { return }
+            onTap!(post)
+        }
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            self.addSubview(buttonsStack)
+            newConstraints()
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+
+        class PostPresenter: FeedViewOutput {
+            func showPost(_ post: Post) {
+                   let postViewController = PostViewController()
+                   postViewController.post = post
+                   navigationController?.pushViewController(postViewController, animated: true)
+               }
+            var navigationController: UINavigationController?
+
+            }
 final class FeedViewController: UIViewController {
     
     var backgroundTask: UIBackgroundTaskIdentifier = .invalid
@@ -51,12 +156,6 @@ final class FeedViewController: UIViewController {
         super.viewDidAppear(animated)
         print(type(of: self), #function)
         
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        ​endBackgroundTask()
-        print(type(of: self), #function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -123,21 +222,5 @@ final class FeedViewController: UIViewController {
             return
         }
         postViewController.post = post
-    }
-}
-
-extension FeedViewController {
-    func registerBackgroundTask() {
-        print("Background task started.")
-        backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
-            self?.​endBackgroundTask()
-        }
-        assert(backgroundTask != .invalid)
-    }
-
-    func ​endBackgroundTask() {
-        print("Background task ended.")
-        UIApplication.shared.endBackgroundTask(backgroundTask)
-        backgroundTask = .invalid
     }
 }
