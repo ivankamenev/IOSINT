c82d1abe52451e20ee9f3ad57370fd4c3a26f98e
Ivan Kamenev <lkamenev92@gmail.com>
2021-05-09 13:29:59 +0300

    refresh project

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -10,44 +10,26 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window:UIWindow?
-    var appCoordinator: MainCoordinator?
-    var vc = MainTabBarController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-//        window?.rootViewController = MainTabBarController()
-//        window?.makeKeyAndVisible()
-        appCoordinator = MainCoordinator(window: window!, vc: vc)
-        appCoordinator?.start()
-        return true
-        
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        print(type(of: self), #function)
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        print(type(of: self), #function)
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        print(type(of: self), #function)
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        print(type(of: self), #function)
-    }
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        print(type(of: self), #function)
+        // Override point for customization after application launch.
         return true
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        print(type(of: self), #function)
-        return true
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
 }
+
--- Navigation/Profile/PhotosTableViewCell.swift
+++ Navigation/Profile/PhotosTableViewCell.swift
@@ -2,7 +2,7 @@
 //  PhotosTableViewCell.swift
 //  Navigation
 //
-//  Created by  Ivan Kamenev on 16.02.2021.
+//  Created by Ivan Kamenev on 03.02.2021.
 //  Copyright © 2021 Artem Novichkov. All rights reserved.
 //
 
@@ -10,108 +10,77 @@ import UIKit
 
 class PhotosTableViewCell: UITableViewCell {
     
-    private let stackView: UIStackView = {
+    private let photoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
+        label.numberOfLines = 1
+        label.text = "Photos"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let arrowImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(systemName: "arrowshape.turn.up.right")
+        imageView.tintColor = UIColor.black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let photosStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
-        stackView.spacing = 8
         stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-    
-    private let textLabelPhotos: UILabel = {
-        let text = UILabel()
-        text.translatesAutoresizingMaskIntoConstraints = false
-        text.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        text.text = "Photos"
-        text.tintColor = .black
-        return text
-    }()
-    
-    private let imagePhotoView: UIImageView = {
-        let imagePhoto = UIImageView()
-        imagePhoto.clipsToBounds = true
-        imagePhoto.layer.cornerRadius = 6
-        imagePhoto.image = #imageLiteral(resourceName: "image 1")
-        return imagePhoto
-    }()
-    private let imagePhotoView1: UIImageView = {
-        let imagePhoto = UIImageView()
-        imagePhoto.clipsToBounds = true
-        imagePhoto.layer.cornerRadius = 6
-        imagePhoto.image = #imageLiteral(resourceName: "image 8")
-        return imagePhoto
-    }()
-    private let imagePhotoView2: UIImageView = {
-        let imagePhoto = UIImageView()
-        imagePhoto.clipsToBounds = true
-        imagePhoto.layer.cornerRadius = 6
-        imagePhoto.image = #imageLiteral(resourceName: "image 19")
-        return imagePhoto
-    }()
-    private let imagePhotoView3: UIImageView = {
-        let imagePhoto = UIImageView()
-        imagePhoto.clipsToBounds = true
-        imagePhoto.layer.cornerRadius = 6
-        imagePhoto.image = #imageLiteral(resourceName: "image 20")
-        return imagePhoto
-    }()
-    
-    private let arrowImage: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(named: "arrow.forward")
-        image.tintColor = .black
-        return image
-    }()
-    
-   
-    
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        setupView()
-        
-        
-        
+        backgroundColor = .white
+        setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    private func setupView() {
+    private func setupViews() {
+        contentView.addSubviews(photoLabel, arrowImageView, photosStackView)
         
-        contentView.addSubviews(stackView, textLabelPhotos, arrowImage)
-        stackView.addArrangedSubview(imagePhotoView)
-        stackView.addArrangedSubview(imagePhotoView1)
-        stackView.addArrangedSubview(imagePhotoView2)
-        stackView.addArrangedSubview(imagePhotoView3)
+        let imageOne = UIImageView(image: #imageLiteral(resourceName: "image 7"))
+        imageOne.contentMode = .scaleToFill
+        let imageTwo = UIImageView(image: #imageLiteral(resourceName: "image 20"))
+        imageTwo.contentMode = .scaleToFill
+        let imageThree = UIImageView(image: #imageLiteral(resourceName: "image 1"))
+        imageThree.contentMode = .scaleToFill
+        let imageFour = UIImageView(image: #imageLiteral(resourceName: "samurai"))
+        imageFour.contentMode = .scaleToFill
+
+        photosStackView.addArrangedSubview(imageOne)
+        photosStackView.addArrangedSubview(imageTwo)
+        photosStackView.addArrangedSubview(imageThree)
+        photosStackView.addArrangedSubview(imageFour)
+
+        
         let constraints = [
+            photoLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
+            photoLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
             
-            textLabelPhotos.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
-            textLabelPhotos.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            arrowImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            arrowImageView.centerYAnchor.constraint(equalTo: photoLabel.centerYAnchor),
             
-            arrowImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
-            arrowImage.centerYAnchor.constraint(equalTo: textLabelPhotos.centerYAnchor),
-            
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -12),
-            stackView.topAnchor.constraint(equalTo: textLabelPhotos.bottomAnchor, constant: 12),
-            stackView.heightAnchor.constraint(equalToConstant: (UIScreen.main.bounds.width - 12 * 2) / 4)
-            
+            photosStackView.topAnchor.constraint(equalTo: photoLabel.bottomAnchor, constant: 12),
+            photosStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            photosStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            photosStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12),
+            photosStackView.heightAnchor.constraint(equalToConstant: 100)
         ]
         
         NSLayoutConstraint.activate(constraints)
     }
 
 }
-
-
-
-
-
-
