09bca79e63743d119bcc89a2c5b9583f1070cb00
Ivan Kamenev <lkamenev92@gmail.com>
2021-06-08 20:29:21 +0300

    DZ2 JSON.Codable.Модель данных

--- Navigation/Network/NetworkService.swift
+++ Navigation/Network/NetworkService.swift
@@ -29,4 +29,31 @@ struct NetworkService {
         
         task.resume()
     }
+    
+    static func infoDataTask(url: URL, block: @escaping (Data) -> Void) {
+            let task = session.dataTask(with: url) { data, response, error in
+                print("check")
+
+                guard error == nil else {
+                    print(error.debugDescription)
+                    return
+                }
+
+                guard let httpResponse = response as? HTTPURLResponse,
+                      httpResponse.statusCode == 200 else { return }
+
+                guard let info = data else { return }
+
+                if let result = String(data: info, encoding: .utf8) {
+                    print(result)
+                }
+
+                DispatchQueue.main.async {
+                    block(info)
+                }
+            }
+
+            task.resume()
+        }
+
 }
