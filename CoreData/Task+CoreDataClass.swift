a0a3020bf0aa24207f591c0ebf324f9f8a3f7ba2
Ivan Kamenev <lkamenev92@gmail.com>
2021-06-29 14:17:35 +0300

    done task CoreData

---
+++ CoreData/Task+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Task+CoreDataClass.swift
+//  Navigation
+//
+//  Created by  Ivan Kamenev on 28.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PostEntity)
+public class PostEntity: NSManagedObject {
+
+}
--- Navigation/MainCoordinator.swift
+++ Navigation/MainCoordinator.swift
@@ -12,16 +12,18 @@ import UIKit
 class MainCoordinator: Coordinator {
     var navigationController: UINavigationController?
     var tabBarController: UITabBarController?
+    private let stack: CoreDataStack
     
-    init(navigationController: UINavigationController?, tabBarController: UITabBarController?) {
+    init(navigationController: UINavigationController?, tabBarController: UITabBarController?, stack: CoreDataStack) {
         self.navigationController = navigationController
         self.tabBarController = tabBarController
+        self.stack = stack
     }
     
     func start() {
         let profileVC = LogInViewController()
         let profileNavigationController = UINavigationController(rootViewController: profileVC)
-        let profileCoordinator = ProfileCoordinator(navigationController: profileNavigationController, tabBarController: nil)
+        let profileCoordinator = ProfileCoordinator(navigationController: profileNavigationController, tabBarController: nil, stack: stack)
         let profile = UITabBarItem(title: "Profile", image:UIImage(systemName: "person") , tag: 0)
         profileVC.delegate = LoginInspector()
         profileVC.coordinator = profileCoordinator
@@ -32,7 +34,12 @@ class MainCoordinator: Coordinator {
         let feed = UITabBarItem(title: "Feed", image: UIImage(systemName: "info.circle"), tag: 1)
         feedVC.coordinator = FeedCoordinator(navigationController: feedNavigationController, tabBarController: nil)
         feedVC.tabBarItem = feed
+        
+        let likesVC = LikesViewController(stack: stack)
+        let likesNavigationController = UINavigationController(rootViewController: likesVC)
+        let likes = UITabBarItem(title: "Likes", image: UIImage(systemName: "suit.heart"), selectedImage: nil)
+        likesVC.tabBarItem = likes
 
-        tabBarController?.viewControllers = [profileNavigationController, feedNavigationController]
+        tabBarController?.viewControllers = [profileNavigationController, feedNavigationController, likesNavigationController]
     }
 }
